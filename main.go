df2714325961cb19786a15917a312d4f6c611430
Reileen00 <business.rout001@gmail.com>
2025-08-07 22:05:53 +0530

    e

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Println("jellp")
+import (
+	"fmt"
+	"log"
+
+	"github.com/Reileen00/FILE/p2p"
+)
+
+func main() {
+	tr := p2p.NewTCPTransport(":3000")
+	if err:= tr.ListenAndAccept();err != nil{
+		log.Fatal(err)
+	}
+	select()
 }
